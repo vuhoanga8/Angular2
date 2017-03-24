0d94b5f053d99058ec0b12d06255fbae67e09df8
vuhoanga8 <hoangvuminh1994@gmail.com>
2017-03-24 22:53:40 +0700

    update master

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
     template: `
         <h1> Hello Hoang </h1>
         <my-tutorial></my-tutorial>
+        <plus-component></plus-component>
     `,
 })
 export class AppComponent { 
