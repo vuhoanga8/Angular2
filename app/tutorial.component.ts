864fbc3d4bdaec44469cb3551055b2a44fbf17c9
vuhoanga8 <hoangvuminh1994@gmail.com>
2017-03-24 23:16:19 +0700

    binding class and style

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -3,9 +3,15 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-tutorial',
     template: 
-        '<h2> tutorial component </h2>'
+        `<h2> tutorial component </h2>
+        <h3 [class.redColor]="applyClass">apply applyClass</h3>
+        <h4 [style.color]="blueColor?'blue':'orange'">Apply Style</h4>
+        `
         ,
+    styles:[`.redColor{color: red}
+        `]
 })
 export class TutorialComponent{
-
+    public applyClass = true;
+    public blueColor = false;
 }
