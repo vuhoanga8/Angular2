0e0d8d90b4d60ba0c5bd783dbd9a19626cb9e92f
Vũ Minh Hoàng <V Minh Hong>
2017-03-27 16:04:03 +0700

    validate form

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
+    styleUrls:['./app/app.component.css']
 })
 export class AppComponent {
+    public cities=[{Id:1, Name:"Ha noi"}, {Id:2, Name: "HCM"},];
     onSubmit(value:any){
         console.log(value);
     }
