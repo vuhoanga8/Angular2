122356e8b52adfc72ac2b2e14bcb2fd43dced9d4
Vũ Minh Hoàng <V Minh Hong>
2017-03-27 15:08:07 +0700

    form in angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1> Hello {{title | lowercase}} </h1>
-        <p>Date today: {{today | date: 'shortDate'}}</p>
-        <p> Percent: {{percentNumber | percent}}</p>
-        <p> e: {{e | number}}
-        <my-tutorial></my-tutorial>
-    `,
+    templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
-    public title = "Vu Minh Hoang";
-    public today =  Date.now();
-    public percentNumber= 1.3495;
-    public e:number= 2.1236489547;
+    onSubmit(value:any){
+        console.log(value);
+    }
 }
 
