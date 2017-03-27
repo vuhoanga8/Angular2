1700ebe12568bbda525f6d6e167bce82068e7d5e
Vũ Minh Hoàng <V Minh Hong>
2017-03-27 10:07:30 +0700

    lession 11

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -1,17 +1,26 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    template: 
-        `<h2> {{title}} </h2>
-        first name <input type ="text" [(ngModel)] = "fname" /> 
-        last name <input type ="text" [(ngModel)] = "lname" /> 
-        <br>
-        Full name : {{fname}} {{lname}}
+    template:
+    `<h2> {{title}} </h2>
+        <h3 *ngIf="showLineIf">This ngIf directive line.</h3>
+        <div [ngSwitch]="color">
+            <p *ngSwitchCase="'red'">This line color is red</p>
+            <p *ngSwitchCase="'blue'">This line color is blue</p>
+            <p *ngSwitchCase="'green'">This line color is green</p>
+            <p *ngSwitchDefault>Invalid color</p>
+        </div>
+        <ul>
+            <li *ngFor= "let color of colors">{{color}}</li>
+        </ul>
         `
-        ,
-    
+    ,
+
 })
-export class TutorialComponent{
-    title = "this is tutorial component"
+export class TutorialComponent {
+    public showLineIf = false;
+    public color = "123";
+
+    public colors: string[] = ["red", "green", "blue"];
 }
