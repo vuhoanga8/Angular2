5d0e00b7dbb685eee86cc88cddeeb4249f55c35a
Vũ Minh Hoàng <V Minh Hong>
2017-03-27 14:12:47 +0700

    lession 13

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TutorialComponent } from './tutorial.component';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1> Hello {{title}} </h1>
-        <my-tutorial></my-tutorial>
+        <input type="text" #textName (keyup)="0" />
+        <p>Agree number: {{agree}}. Disagree: {{disgree}}</p>
+        <button (click)="changeName()">Change name</button>
+        <my-tutorial *ngFor="let person of names" [name]="person" (onVote)="parentVote($event)"></my-tutorial>
     `,
 })
-export class AppComponent { 
+export class AppComponent {
     public title = "Vu Minh Hoang"
+
+    public agree: number = 0;
+    public disgree: number = 0;
+    public names = ['MR A', 'Mr B', 'Mr C', 'Mr D'];
+
+    @ViewChild(TutorialComponent)
+    private tutorialComponent: TutorialComponent;
+    
+
+    parentVote(agree: boolean) {
+        if (agree) this.agree++;
+        else this.disgree++;
+    }
+
+    changeName(){
+        this.tutorialComponent.setName("change name to this");
+    }
+
 
 }
 
--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -1,30 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
     template:
     `<h2> {{title}} </h2>
-    <p [ngClass]="{classOne:cone,classTwo:ctwo}">This is ngClass apply style</p>
-    <button (click)="toggle()">Toggle</button>
-    <p [ngStyle]="{'font-style': style, 'font-size':size}">This paragraph will be apply to ngStyle</p>
-        `
+    <p>Child component: {{name}}</p>
+
+    <button [disabled]="voted" (click)="vote(true)">Agree</button>
+    <button [disabled]="voted" (click)="vote(fasle)">Disgree</button>
+
+    Result:  {{voted}}
+     `
     ,
-    styles:[`
-        .classOne{
-            color: yellow;
-        }
-        .classTwo{
-            background-color: black;
-        }
-    `]
+    
 })
 export class TutorialComponent {
-    public cone = true;
-    public ctwo = true;
-    public style = "italic";
-    public size = "30px";
-    toggle(){
-        this.cone = !this.cone;
-        this.ctwo = !this.ctwo;
+    @Input() name:string;
+    @Output() onVote = new EventEmitter<boolean>();
+
+
+    public voted:boolean = false;
+
+    vote(agree:boolean){
+        this.voted = true;
+        this.onVote.emit(agree);
+    }
+
+    setName(name: string){
+        this.name = name;
     }
 }
